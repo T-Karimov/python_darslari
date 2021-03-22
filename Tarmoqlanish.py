ff70f7f378a2fa9a3149c6da010481ba730fe234
T-Karimov <80973756+T-Karimov@users.noreply.github.com>
2021-03-22 14:21:42 +0500

    Add files via upload

---
+++ Tarmoqlanish.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 15 21:58:34 2021
+
+@author: TEMUR
+"""
+
+#avtolar = ["audi","bmw","volvo","kia","hundai"]
+#for avto in avtolar:
+     #if avto == 'bmw':
+     #     print(avto.upper())
+     #else:
+          # print(avto.title())
+#ism='Temur'
+#ism.lower()=="Temur"           
+#ism=input("isminggizni kiriting\n  ")
+#if ism.lower() != 'ali':
+ #   print(f"Uzr {ism.title()} Alini ni kutyabmiz")
+#else:
+    #print("Salom Ali")
+#qoshish=float(input("23*2 nechiga teng? \n>>> "))
+#if qoshish!=66:
+    #print("javob xato")
+#yosh=int(input("yoshinggiz nechida \n>>> "))   
+#if yosh >=18:
+ #   print("Hush kelibsiz")
+#else:
+    #print("Kirish mumkin emas")
+#login=input("Logenni kiriting  ")
+#if len(login) <=5:
+#    print("5 tadan ko'p harf bo'lishi kerak")
+#yosh=int(input("yilinggizni kiriting  "))
+#if 2021-yosh<18:
+ #   print(f"Yoshiz {2021-yosh} da ekan kirish mumkin emas")
+#else:
+ #   print("Hush kelibsiz")
+#yosh = int(input("Yoshingizni kiriting  "))
+#f yosh<65: print("Siz risk guruhidan ekansiz")    
+    
+#x ,y = 65,50
+#print("x>y") if x>y else print("x<y")
+#cars=["toyota","kia","mazda","gm","tesla"]
+#for car in cars:
+  #  if car.lower() !='gm':
+ #       print(car.title())
+#    else:
+        #print(car.upper())
+        
+#ism=input("Assalomu alekum ismingizni kiriting  ")
+#if ism.lower() =='admin':
+ #    print(f"Hush kelibsiz {ism.title()}")
+#else:
+#         print(f"Hush kelibsiz {ism.title()}.\nFoydalanuvchilarni ko'rasizmi?")
+#x=float(input("Birinchi sonni kiriting "))
+#y=float(input("ikkinchi sonni kiriting "))
+#if x==y: print(f"bu sonlar teng {x}={y}")
+#son=float(input("Istalgan sonni kiriting "))
+#print(son**(1/2)) if son>0 else print("Musbat sonni kiriting")
+
+
+#avtolar = ["mers","bmw","tesla","hundai","toyota"]
+#for avto in avtolar:
+#    if avto == 'bmw':
+ #       print(avto.upper())
+ #   else:
+#        print(avto.title())
+#ism=input("Ismingizni kiriting\n>>> ")
+#if ism.lower()=='ali':
+#    print(f" Hush kelibsiz, {ism.title()},.\n Ro'yhatni tekshirasizmi")
+#else:
+#    print(F"Hush kelibsiz,{ism.title()}")
+    
+#familya=input("Familyanggizni kiriting\n>>>")
+#if familya.lower() != "temur":
+#    print(f" Hush kelibsiz {familya.title()}")
+#else:
+#    print(f"Assalomu alekum {familya.title()} \n Ro'yhatni tekshirasizmi")
+    
+#son=float(input("12*3 nechiga teng\n>>>"))
+#if son != 36:
+#    print("Javob hato")
+#else:
+#    print(f"{son} to'g'ri javob")
+#yosh=int(input("yoshinggizni kiriting\n>>> "))
+#if yosh <= 18:
+#    print("Kirish mumkin emas")
+#else:
+#    print("Hush kelibsiz")
+    
+    
+#login=input("Logenni kiriting ")
+#if len(login)<=5:
+#    print("Login 5 harifdan ko'p bo'lishi shart")
+
+#yosh=int(input("Tug'ilgan yilinggizni kiriting "))    
+#if 2021-yosh <18:
+#    print(f"Sizni yoshinggiz {2021-yosh} da ekan ")
+#    print("Kirish mumkin emas")
+#else:
+#    print("Hush kelibsiz")
+    
+    
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+
+
+
